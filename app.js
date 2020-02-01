48501d753e2351f3a4778e0eb5a6cfa47477be27
polly <miru11104627@gmail.com>
2020-01-31 21:53:50 -0500

    add controllers to the app

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
 // get current month
 let month=new Date().getMonth();
+let currentYear=new Date().getFullYear();
 const monthArr=['January','February','March','April','May','June','July','August','September','October','Novenber','December']
-$(".budget__title--month").html(monthArr[month]);
+$(".budget__title--month").html(monthArr[month]+' '+currentYear);
+
+//budgetController
+let budgetController=(function() {
+
+
+})();
+
+//UI con
+let UIController=(function(){
+
+})();
+
+//global app controller
+let controller=(function(bugetCtrl,UICtrl){
+
+   let crlAddItem=function(){
+       //get input data
+        //add item to buget controller
+        //add item to interface
+        //caculate budget
+        //display budget on homepage
+        console.log("test!!!!!!1");
+   }
+
+// jquery with callback
+    $(".add__btn").click(crlAddItem);
+    
+    $(document).keypress(function(e){
+       //type enter button
+        if (e.keyCode===13 || e.which===13){
+            crlAddItem();
+        }
+
+    });
+})(budgetController,UIController);
