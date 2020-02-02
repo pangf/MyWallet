7a7f0d65c79b5d8cbdb674edfb6db36d3f4d7bae
polly <miru11104627@gmail.com>
2020-02-02 15:42:41 -0500

    add UI module

--- app.js
+++ app.js
@@ -18,129 +18,204 @@ const monthArr = [
 ];
 $('.budget__title--month').html(monthArr[month] + ' ' + currentYear);
 
-//budgetController
+// BUDGET CONTROLLER
 var budgetController = (function() {
-    var Expense=function(id,description,value){
-        this.id=id;
-        this.description=description;
-        this.value=value;
+	var Expense = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 
-    };
-    var Income=function(id,description,value){
-        this.id=id;
-        this.description=description;
-        this.value=value;
+	var Income = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 
-    };
-    var data = {
-        allItems: {
-            exp: [],
-            inc: []
-        },
-        totals: {
-            exp: 0,
-            inc: 0
-        }
-        
-    };
-    return {
-        addItem: function(type,des,val){
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	};
 
-            var newItem,ID;
-            var objLength=data.allItems[type].length;
-            console.log("length"+objLength);
-            
-            if (objLength>0){
-                ID= data.allItems[type][objLength-1].id+1;
-                console.log("id"+ID);
-            }else{
-                ID=0;
-            }
-            if(type==="exp"){
-                newItem=new Expense(ID,des,val);
-                console.log(newItem);
+	return {
+		addItem: function(type, des, val) {
+			var newItem, ID;
 
-            }else{
-                newItem= new Income(ID,des,val);
-                console.log(newItem);
-            }
-          data.allItems[type].push(newItem);
-          console.log(data.allItems[type].length);
-          
-          return newItem;
-          
+			//[1 2 3 4 5], next ID = 6
+			//[1 2 4 6 8], next ID = 9
+			// ID = last ID + 1
 
-            
+			// Create new ID
+			if (data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+			} else {
+				ID = 0;
+			}
 
-        }
-    }
-    
+			// Create new item based on 'inc' or 'exp' type
+			if (type === 'exp') {
+				newItem = new Expense(ID, des, val);
+			} else if (type === 'inc') {
+				newItem = new Income(ID, des, val);
+			}
 
+			// Push it into our data structure
+			data.allItems[type].push(newItem);
+
+			// Return the new element
+			return newItem;
+		}
+	};
 })();
 
-
-
-
-//UI controller
+// UI CONTROLLER
 var UIController = (function() {
-	//easy for enhance
-	var DomStrings = {
+	var DOMstrings = {
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputBtn: '.add__btn'
+		inputBtn: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list',
+		// budgetLabel: '.budget__value',
+		// incomeLabel: '.budget__income--value',
+		// expensesLabel: '.budget__expenses--value',
+		// percentageLabel: '.budget__expenses--percentage',
+		// container: '.container',
+		// expensesPercLabel: '.item__percentage',
+		// dateLabel: '.budget__title--month'
+	};
+
+	var formatNumber = function(num, type) {
+		var numSplit, int, dec, type;
+		/*
+            + or - before number
+            exactly 2 decimal points
+            comma separating the thousands
+
+            2310.4567 -> + 2,310.46
+            2000 -> + 2,000.00
+            */
+
+		num = Math.abs(num);
+		num = num.toFixed(2);
+
+		numSplit = num.split('.');
+
+		int = numSplit[0];
+		if (int.length > 3) {
+			int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3); //input 23510, output 23,510
+		}
+
+		dec = numSplit[1];
+
+		return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+	};
+
+	var nodeListForEach = function(list, callback) {
+		for (var i = 0; i < list.length; i++) {
+			callback(list[i], i);
+		}
 	};
 
 	return {
 		getInput: function() {
-			//return object makesure function can be used outside of the scorpe
 			return {
-				type: $(DomStrings.inputType).val(),
-				value: $(DomStrings.inputValue).val(),
-				description: $(DomStrings.inputDescription).val()
+				
+				type: $(DOMstrings.inputType).val(),
+				value: $(DOMstrings.inputValue).val(),
+				description: $(DOMstrings.inputDescription).val()
 			};
 		},
-		getDomStrings: function() {
-			// makesure function can be used outside of the scorpe
-			return DomStrings;
+
+		addListItem: function(obj, type) {
+			var html, newHtml, element;
+			// Create HTML string with placeholder text
+
+			if (type === 'inc') {
+				element = DOMstrings.incomeContainer;
+
+				html =
+					'<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			} else if (type === 'exp') {
+				element = DOMstrings.expensesContainer;
+
+				html =
+					'<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+
+			// Replace the placeholder text with some actual data
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
+
+			// Insert the HTML into the DOM
+			$(element).append(newHtml);
+		},
+
+		getDOMstrings: function() {
+			return DOMstrings;
 		}
 	};
 })();
 
-//global app controller
-var controller = (function(bugetCtrl, UICtrl) {
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
 	var setupEventListeners = function() {
-		var Dom = UICtrl.getDomStrings();
-		// jquery with callback
-		$(Dom.inputBtn).click(ctrlAddItem);
-		$(document).keypress(function(e) {
-			//type enter button
-			if (e.keyCode === 13 || e.which === 13) {
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+			if (event.keyCode === 13 || event.which === 13) {
 				ctrlAddItem();
 			}
 		});
+
+		// document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+		// document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 	};
+
 
 	var ctrlAddItem = function() {
-		//get input data
-        var input,newItem;
-		input = UIController.getInput();
-		console.log(input);
-		//add item to bugetcontroller
-		newItem = budgetController.addItem(input.type, input.description, input.value);
-        console.log(newItem);
-		//add item to interface
-		//caculate budget
-		//display budget on homepage
-		
-	};
+        var input, newItem;
+        
+        // 1. Get the field input data
+        input = UICtrl.getInput();        
+        
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-	return {
-		init: function() {
-			setupEventListeners();
-			console.log('app statrt');
-		}
-	};
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
+
+            // 4. Clear the fields
+          
+
+            // 5. Calculate and update budget
+           
+            
+            // 6. Calculate and update percentages
+            
+        }
+    };  
+    return {
+        init: function() {
+            console.log('Application has started.');
+           
+            setupEventListeners();
+        }
+    };
+    
 })(budgetController, UIController);
+
 
 controller.init();
